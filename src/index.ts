d263ddc76c28594b2f58b00011ac6e9efefee7f4
shenoac <shenoachee@gmail.com>
2024-08-25 15:12:09 +0200

    Switch to http on Heroku

--- src/index.ts
+++ src/index.ts
@@ -2,8 +2,6 @@ import express from 'express';
 import dotenv from 'dotenv';
 import studentRoutes from './routes/students';
 import logRequest from './middleware/logRequest';
-import fs from 'fs';
-import https from 'https';
 import { WebSocketServer } from 'ws';
 
 dotenv.config();
@@ -11,30 +9,23 @@ dotenv.config();
 const app = express();
 const port = process.env.PORT || 3000;
 
-// Middleware
 app.use(express.json());
 app.use(logRequest);
 app.use(express.static('public'));
 
-
-// Routes
 app.use('/api/students', studentRoutes);
 
 app.get('/', (req, res) => {
   res.send('Welcome to the Express App!');
 });
 
-// Load SSL certificates for HTTPS
-const privateKey = fs.readFileSync(__dirname + '/privatekey.pem', 'utf8');
-const certificate = fs.readFileSync(__dirname + '/cert.pem', 'utf8');
-const credentials = { key: privateKey, cert: certificate };
-
-// Create an HTTPS server with WebSocket support
-const server = https.createServer(credentials, app);
+// Create HTTP server instead of HTTPS
+const server = app.listen(port, () => {
+  console.log(`Server is running on http://localhost:${port}`);
+});
 
 // Initialize WebSocket server
 const wss = new WebSocketServer({ server });
-
 
 wss.on('connection', (ws) => {
   console.log('New WebSocket connection established');
@@ -48,11 +39,5 @@ wss.on('connection', (ws) => {
     console.log('WebSocket connection closed');
   });
 });
-
-// Start the server
-server.listen(port, () => {
-  console.log(`Server is running securely on https://localhost:${port}`);
-});
-
 
 export default app;
